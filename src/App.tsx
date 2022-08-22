8718d72a9f12efbc4c297d0694655c6bc0cdc3e2
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-22 22:35:31 +0300

    feat: add useState and useEffect - func fetchProducts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { Product } from './components/Product'
-import {products} from './data/products'
+// import {products} from './data/products'
+import { IProduct } from './models'
 
 function App() {
+
+  const [products, setProducts] = useState<IProduct[]>([])
+
+  async function fetchProducts() {
+    const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=6')
+    // console.log('response', response);
+    setProducts(response.data)
+  }
+
+  useEffect(() => {
+    // console.log('effect');
+    fetchProducts()
+  }, [])
 
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
       {products.map(product => <Product product={product} key={product.id} />)}
-      {/* <Product product={products[0]} /> */}
-      {/* <Product product={products[1]} /> */}
     </div>
   )
 }
