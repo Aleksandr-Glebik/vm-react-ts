102142d48c000f837db97b3721f6f41e6f1f64e3
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-22 21:14:15 +0300

    feat: add hook useState - count, setCount

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div >
-      <h1 className="text-3xl font-bold">
-        Start project - react-ts
+    <div style={{textAlign: 'center', marginTop: '30px'}}>
+      <h1 className="text-3xl font-bold py-4">
+        {count}
       </h1>
+      <button className={'py-2 px-4 border'} onClick={() => {setCount(count + 1)}}>Click ++</button>
+      <button className={'py-2 px-4 border'} onClick={() => {setCount(count - 1)}}>Click --</button>
     </div>
   )
 }
