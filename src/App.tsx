f2edcf375c72aa9514c08368df26e97ae2e03b7c
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-23 21:00:40 +0300

    feat: add ModalContext, ModalState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react'
 import { Product } from './components/Product'
 import {useProducts} from './hooks/products'
 import { Loader } from './components/Loader'
@@ -6,14 +6,17 @@ import { ErrorMessage } from './components/ErrorMessage'
 import {Modal} from './components/Modal'
 import { CreateProduct } from './components/CreateProduct'
 import { IProduct } from './models'
+import { ModalContext } from './context/ModalContext'
 
 function App() {
 
   const {products, loading, error, addProduct} = useProducts()
-  const [modal, setModal] = useState(false)
+  // const [modal, setModal] = useState(false)
+  const {modal, open, close} = useContext(ModalContext)
 
   const createHandler = (product: IProduct) => {
-    setModal(false)
+    // setModal(false)
+    close()
     addProduct(product)
   }
 
@@ -23,13 +26,13 @@ function App() {
       { error && <ErrorMessage error={error}/> }
       { products.map(product => <Product product={product} key={product.id} />) }
 
-      {modal && <Modal title='Create new Product' onClose={() => setModal(false)}>
+      {modal && <Modal title='Create new Product' onClose={close}>
         <CreateProduct onCreate={createHandler}/>
       </Modal>
       }
       <button
         className='fixed bottom-8 right-8 px-4 py-2 rounded-full bg-red-700 text-white text-2xl'
-        onClick={() => setModal(true)}
+        onClick={open}
       >
         +
       </button>
